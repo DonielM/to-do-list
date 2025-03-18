8959e07f002095a4d22021db20c9888cee5fae14
DonielM <DonielMarekera@outlook.com>
2025-03-18 15:28:32 +0000

    feat: the input field is cleared when a task is added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,13 @@
 const todoList = [];
 
+// this functions adds tasks typed in the input to the todoList array
 function addTask() {
   const input = document.querySelector(".js-task-input");
   const taskInput = input.value;
   todoList.push(taskInput);
   console.log(todoList);
+
+  input.value = "";
 }
 
 document.querySelector(".js-add-task-button").addEventListener("click", () => {
